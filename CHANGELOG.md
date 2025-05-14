66768e6f46db97317ae700bcd92e7558dfe3894b
Trevor Plassman <trevor@brasstacksweb.com>
2025-05-14 20:57:53 +0000

    updating changelog and plugin file to include version bump for null safe operator

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,3 +5,6 @@
 
 ## 2.0.0
 - Adding support for Craft CMS v5
+
+## 2.0.1
+- Updated Blocker service to use null-safe operator when accessing previous exception.
--- src/IPBlocker.php
+++ src/IPBlocker.php
@@ -27,7 +27,7 @@ use yii\web\HttpException;
  */
 class IPBlocker extends Plugin
 {
-    public string $schemaVersion = '2.0.0';
+    public string $schemaVersion = '2.0.1';
     public bool $hasCpSection = true;
     public bool $hasCpSettings = true;
 
