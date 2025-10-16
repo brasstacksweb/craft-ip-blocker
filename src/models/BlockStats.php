8331a16fc96a2c1d6d5bcba7018bae07f16eceb1
Trevor Plassman <trevor@brasstacksweb.com>
2025-10-16 19:06:35 +0000

    model and template refactoring to simplify and consolidate model attributes and references.  Adding sorting functionality to stats table columns for attempts and blocks

--- src/models/BlockStats.php
+++ src/models/BlockStats.php
@@ -12,4 +12,25 @@ class BlockStats extends Model
     public \DateTime $firstBlocked;
     public \DateTime $expires;
     public bool $isActive;
+
+    public function getAttributes($names = null, $except = [], $onlyChanged = false): array
+    {
+        return array_merge(parent::getAttributes($names, $except, $onlyChanged), [
+            'firstBlocked' => $this->firstBlocked->format('Y-m-d H:i:s'),
+            'expires' => $this->expires->format('Y-m-d H:i:s'),
+            'isActive' => $this->isActive ? 'Yes' : 'No',
+        ]);
+    }
+
+    public function attributeLabels(): array
+    {
+        return [
+            'ip' => 'IP Address',
+            'reason' => 'Reason',
+            'count' => 'Number of Attempts',
+            'firstBlocked' => 'First Blocked',
+            'expires' => 'Expires',
+            'isActive' => 'Is Active',
+        ];
+    }
 }
